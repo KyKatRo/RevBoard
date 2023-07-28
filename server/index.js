7fdd8b44a0a9b9b4388cfe9e30a3e48bd9f47091
Vinay Kancharla <vinaykancharla7@gmail.com>
2023-07-28 18:20:10 -0400

    Configure KPI route and test database queries

--- server/index.js
+++ server/index.js
@@ -21,12 +21,31 @@ app.use(cors());
 /* routes */
 app.use("/kpi", kpiRoutes);
 
+// Test route
+app.get("/test", (req, res) => {
+	res.send("This works!");
+});
+
 /* node-postgres Setup */
 const PORT = process.env.PORT || 9000;
 
-console.log(`Port: ${PORT}`);
-console.log(`Port: ${process.env.POSTGRES_URL}`);
+// Test server connection
+app.listen(PORT, () => {
+	console.log(`Server has started on ${PORT}`);
+});
 
-const data = await pool.query("SELECT NOW()");
-console.log(data.rows);
+// Test database connection
+const time = await pool.query("SELECT NOW()");
+console.log(time.rows);
+
+// Test database insertion; will insert every time server refreshes
+// const text = "INSERT INTO test(name, age, occupation) VALUES($1, $2, $3)";
+// const values = ["name", age, "occupation"];
+// await pool.query(text, values);
+
+// Test database selection, displays all data from test table
+const selectQuery = "SELECT * FROM test";
+const res = await pool.query(selectQuery);
+console.log(res.rows);
+
 await pool.end();
