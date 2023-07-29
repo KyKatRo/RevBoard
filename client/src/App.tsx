0c33018041d9eb4b6f4f7d1f91a60b2db08e670a
Vinay Kancharla <vinaykancharla7@gmail.com>
2023-07-29 02:28:35 -0400

    Create UI for predictions page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,6 +6,7 @@ import { themeSettings } from "./theme";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "@/scenes/navbar";
 import Dashboard from "@/scenes/dashboard";
+import Predictions from "@/scenes/predictions";
 
 function App() {
 	const theme = useMemo(() => createTheme(themeSettings), []);
@@ -21,13 +22,10 @@ function App() {
 					>
 						<Navbar />
 						<Routes>
+							<Route path='/' element={<Dashboard />} />
 							<Route
-								path='/'
-								element={<Dashboard />}
-							/>
-							<Route
-								path='/predicitons'
-								element={<div>predictions page</div>}
+								path='/predictions'
+								element={<Predictions />}
 							/>
 						</Routes>
 					</Box>
