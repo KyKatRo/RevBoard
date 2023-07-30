8385d8de67d73bda8a2ed2dadb7ae92278b5f83e
Mia Yang <mia.h1.yang@gmail.com>
2023-07-30 00:42:21 -0700

    products table query route

--- server/routes/product.js
+++ server/routes/product.js
@@ -1,15 +1,14 @@
 import express from "express";
 import pool from "../db.js";
-import { products } from "../data/data.js";
 
 const router = express.Router();
 
+const query = 'SELECT p.product_id, p.product_price, e.amount FROM products p, expenses e, manufacturing_cost mc WHERE p.product_id = mc.product_id and mc.expense_id = e.expense_id; ';
+
 router.get("/products", async (req, res) => {
 	try {
-		const data = products;
-		// await pool.query("SELECT * FROM product");
-		// res.json(products.rows);
-		res.status(200).json(data);
+		const data = await pool.query(query);
+		res.status(200).json(data.rows);
 	} catch (err) {
 		res.status(404).json({ message: err.message });
 	}
